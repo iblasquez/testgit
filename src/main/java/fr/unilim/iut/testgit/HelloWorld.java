a7b22a01018b564fa66d3a383167bb92248da76e
Isabelle BLASQUEZ <isabelle.blasquez31@gmail.com>
2017-02-11 16:53:33 +0100

    Ajout Marabout 3 phrases

--- src/main/java/fr/unilim/iut/testgit/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testgit/HelloWorld.java
@@ -9,6 +9,9 @@ public class HelloWorld {
 		
 		System.out.println( "Et si on jouait au FizzBuzz ?" );  
         System.out.println(FizzBuzz.getResult());
+        
+        System.out.println( "Et si on jouait au Marabout ?" );  
+        System.out.println(Marabout.getResult());
 	}
 
 }
