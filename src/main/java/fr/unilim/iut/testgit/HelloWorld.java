e12dbee54fdcb2a46132479d5884bc7576028ca5
Isabelle BLASQUEZ <isabelle.blasquez31@gmail.com>
2017-02-11 17:49:35 +0100

    Ajout Marabout simple 3 mots

--- src/main/java/fr/unilim/iut/testgit/HelloWorld.java
+++ src/main/java/fr/unilim/iut/testgit/HelloWorld.java
@@ -12,6 +12,9 @@ public class HelloWorld {
         
         System.out.println( "Et si on jouait au Marabout ?" );  
         System.out.println(Marabout.getResult());
+        
+        System.out.println( "Et si on jouait au Marabout de manière plus simple ?" );  
+        System.out.println(MaraboutSimple.getResult());
 	}
 
 }
---
+++ src/main/java/fr/unilim/iut/testgit/MaraboutSimple.java
@@ -0,0 +1,9 @@
+package fr.unilim.iut.testgit;
+
+public class MaraboutSimple {
+	
+	public static String getResult() {
+        return "Marabout, Bout de ficelle, Selle de cheval";
+     }
+
+}
